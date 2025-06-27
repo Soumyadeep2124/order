3d35d8b41e4d0860b5c1fdf119abcb2a1316fc71
Soumyadeep Bhowmik <166634787+Soumyadeep2124@users.noreply.github.com>
2025-06-27 09:45:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// const resturant=[];
+// const images=[];
+// const resturant_name[];
+// const foodType=[];
+// const 
+
+// for(let i=0;i<100;i++)
+// {
+//     const obj={};
+//     // obj["image"]=["first,"second"...][Math.random()*10]
+//     obj["rating"]=Math.floor(Math.random()*5+1)
+//     obj["foodtype"]
+// }
